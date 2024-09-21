3d5938668aa69a133efad0b36d0de2ce7f0fef12
sandeepkumar <sandeep.kumar23@antiersolutions.com>
2024-09-21 20:21:08 +0530

    initial commit

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sandeep kumar pal </span>
+            from <span className="purple"> Datia Madhya Pradesh, India.</span>
+            <br />
+            I am currently employed as a FrontEnd developer at Antier Sol.
+            <br />
+            I have completed Bachelor of Engineering (BE) in Electronics and
+            Communication at RGPV University.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Reading Novels
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Watching Movies
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sandeep</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
